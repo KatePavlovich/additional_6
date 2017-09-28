5fe7b30f80b3de3b1a1b31f9f25d29b0b65691a9
kate <asylumhere@mail.ru>
2017-09-29 01:38:50 +0300

    one more

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 module.exports = function zeros(expression) {
 
-	var counter=0;
-	var N;
-var num= parseInt(expression);
+var arr=expression.split(/\*/g);
+console.log(arr);
+var arr2=[];
+
+for (var i=0; i<arr.length; i++){
+if (arr[i].match('/\!/')){
+arr2.push(counterZeros(arr[i]));
+
+}
+else arr2.push(Math.floor(counterZeros(arr[i])/2));
+
+
+
+}
+console.log(arr2);
+
+  function counterZeros(exp){
+  var counter=0;
+  var N;
+var num= parseInt(exp);
 //console.log (num);
 
 
-	N =Math.log(num) / Math.log(5);
-	for (var i=1; i<=N; i++){
+  N =Math.log(num) / Math.log(5);
+  for (var i=1; i<=N; i++){
 counter += num/Math.pow(5,i)
 
 }
  return Math.floor(counter);
+}
+var result = arr2.reduce(function(sum, current) {
+  return sum + current;
+}, 0);
+
+return result;
 }
