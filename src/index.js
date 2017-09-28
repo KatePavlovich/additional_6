0eea30ad8691e3955fd86f2c1df54dec364fb297
kate <asylumhere@mail.ru>
2017-09-28 09:54:24 +0300

    test2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function zeros(expression) {
-  return 1;
+  var ex= String(expression).split('!').map(Number);
+var res1=1;
+
+for (var i=0; i< ex.length; i++){
+	var res1 =res1 * ex[i];
+return res1;
+
+	}
+console.log(res1);
+	var n=5;
+	while (n<res1){
+		
+		
+	 res = res + (res1/n);
+	 
+	 n= n*5;
+}
+ return res;
 }
